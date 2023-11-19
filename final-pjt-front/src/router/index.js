ca1e599af576ad8db57ba679a1d619fc330efc9a
Sooji Kwon <gkdrhd6788@gmail.com>
2023-11-19 17:30:11 +0900

    signup_login_logout

--- final-pjt-front/src/router/index.js
+++ final-pjt-front/src/router/index.js
@@ -1,23 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useCounterStore } from '@/stores/counter'
+import SignUpView from '@/views/SignUpView.vue'
+import LogInView from '@/views/LogInView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/signup',
+      name: 'SignUpView',
+      component: SignUpView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'LogInView',
+      component: LogInView
+    },
   ]
 })
+
+router.beforeEach((to,from)=>{
+  const store = useCounterStore()
+  // if (to.name === 'ArticleView' && !store.isLogin) {
+  //   window.alert('로그인이 필요합니다.')
+  //   return { name : 'LogInView'}
+  // }
+  if ((to.name === 'SignUpView' || to.name=='LogInView') && (store.isLogin)){
+    window.alert('이미 로그인 했습니다.')
+    return { name: 'ArticleView'}
+  }
+})
+
+
+
 
 export default router
